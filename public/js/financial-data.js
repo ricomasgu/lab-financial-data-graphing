83ffec90f1eb5ce69aa99bc06fb1fe611f079526
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-21 19:37:27 +0100

    Iteration 2

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,5 +1,34 @@
+const ctx = document.getElementById('myChart').getContext('2d');
 const url = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
+ 
+
+  
+
+  
+
 axios.get(url)
-.then(responseFromApi => console.log(responseFromApi))
+.then(responseFromApi => {
+    const dataFromApi = responseFromApi.data.bpi;
+
+    const labels = Object.keys(dataFromApi);
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: Object.values(dataFromApi),
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+
+    const myChart = new Chart(ctx, config);
+})
 .catch(error => console.log(error));
