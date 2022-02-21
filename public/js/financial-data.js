43c28c6878ca97657b783e71655f134df9bda0ad
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-21 21:23:19 +0100

    Bonus: Currency

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -26,6 +26,9 @@ const updateChart = (url) => {
                 options: {}
             };
 
+            if(myChart !== undefined){
+                myChart.destroy();
+            }
             myChart = new Chart(ctx, config);
         })
         .catch(error => console.log(error));
@@ -35,21 +38,28 @@ updateChart(baseUrl);
 
 const fromDateInput = document.getElementById("fromDate");
 const toDateInput = document.getElementById("toDate");
+const currencyInput = document.getElementById("currency");
 let fromDate;
 let toDate;
 
 fromDateInput.addEventListener("change", (event) => {
     fromDate = String(event.target.value);
     if(toDate !== undefined && ( toDate.localeCompare(fromDate) > 0 )){
-        myChart.destroy();
-        updateChart(`${baseUrl}?start=${fromDate}&end=${toDate}`);
+        updateChart(`${baseUrl}?start=${fromDate}&end=${toDate}&currency=${currencyInput.value}`);
     }
 });
 
 toDateInput.addEventListener("change", (event) => {
     toDate = String(event.target.value);
     if(fromDate !== undefined && ( toDate.localeCompare(fromDate) > 0 )){
-        myChart.destroy();
-        updateChart(`${baseUrl}?start=${fromDate}&end=${toDate}`);
+        updateChart(`${baseUrl}?start=${fromDate}&end=${toDate}&currency=${currencyInput.value}`);
+    }
+});
+
+currencyInput.addEventListener("change", () => {
+    if(fromDate === undefined || toDate === undefined || ( toDate.localeCompare(fromDate) < 0 )){
+        updateChart(`${baseUrl}?currency=${currencyInput.value}`);
+    } else {
+        updateChart(`${baseUrl}?start=${fromDate}&end=${toDate}&currency=${currencyInput.value}`);
     }
 });
